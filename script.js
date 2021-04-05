3e76457a367e32bb006d9ac3b41a1d2a0534e78a
sayyidshiddiqmasagena <67496823+sayyidshiddiqmasagena@users.noreply.github.com>
2021-04-05 20:50:11 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,120 @@
+$(document).ready(function () {
+  fetch();
+
+  function fetch() {
+    let url = "https://covid19.mathdro.id/api";
+
+    let data = "";
+
+    $.get(url, function (data) {
+      console.log(data.confirmed);
+      console.log(data.recovered);
+      console.log(data.deaths);
+
+      data = `
+          <td>${data.confirmed.value}</td>
+          <td>${data.recovered.value}</td>
+          <td>${data.deaths.value}</td>
+
+          `;
+
+      $("#data").html(data);
+    });
+  }
+
+  fetch1();
+
+  function fetch1() {
+    let url = "https://covid19.mathdro.id/api/countries/Indonesia";
+
+    let dataindonesia = "";
+
+    $.get(url, function (dataindonesia) {
+      console.log(dataindonesia.confirmed);
+      console.log(dataindonesia.recovered);
+      console.log(dataindonesia.deaths);
+
+      dataindonesia = `
+          <td>${dataindonesia.confirmed.value}</td>
+          <td>${dataindonesia.recovered.value}</td>
+          <td>${dataindonesia.deaths.value}</td>
+
+          `;
+
+      $("#dataindonesia").html(dataindonesia);
+    });
+  }
+
+  fetch2();
+
+  function fetch2() {
+    let url = "https://covid19.mathdro.id/api/countries/Singapore";
+
+    let datasingapore = "";
+
+    $.get(url, function (datasingapore) {
+      console.log(datasingapore.confirmed);
+      console.log(datasingapore.recovered);
+      console.log(datasingapore.deaths);
+
+      datasingapore = `
+          <td>${datasingapore.confirmed.value}</td>
+          <td>${datasingapore.recovered.value}</td>
+          <td>${datasingapore.deaths.value}</td>
+
+          `;
+
+      $("#datasingapore").html(datasingapore);
+    });
+  }
+
+  fetch3();
+
+  function fetch3() {
+    let url = "https://covid19.mathdro.id/api/countries/Malaysia";
+
+    let datamalaysia = "";
+
+    $.get(url, function (datamalaysia) {
+      console.log(datamalaysia.confirmed);
+      console.log(datamalaysia.recovered);
+      console.log(datamalaysia.deaths);
+
+      datamalaysia = `
+          <td>${datamalaysia.confirmed.value}</td>
+          <td>${datamalaysia.recovered.value}</td>
+          <td>${datamalaysia.deaths.value}</td>
+
+          `;
+
+      $("#datamalaysia").html(datamalaysia);
+    });
+  }
+
+  fetch4();
+
+  function fetch4() {
+    let url = "https://covid19.mathdro.id/api/countries/Burma";
+
+    let datamyanmar = "";
+
+    $.get(url, function (datamyanmar) {
+      console.log(datamyanmar.confirmed);
+      console.log(datamyanmar.recovered);
+      console.log(datamyanmar.deaths);
+
+      datamyanmar = `
+          <td>${datamyanmar.confirmed.value}</td>
+          <td>${datamyanmar.recovered.value}</td>
+          <td>${datamyanmar.deaths.value}</td>
+
+          `;
+
+      $("#datamyanmar").html(datamyanmar);
+    });
+  }
+});
+
 function average() {
   A = parseFloat(document.getElementById("firstNum").value);
   B = parseFloat(document.getElementById("secondNum").value);
